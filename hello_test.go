19479dc9160565b56342a730a84454d158182386
Longoria Yu <longoria.yu@thecarousell.com>
2022-08-31 14:59:21 +0800

    Refactored the test code to avoid the duplicate code

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,27 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Longoria")
-	want := "Hello, Longoria"
+	t.Run("saying hello to people", func(t *testing.T) {
+		actual := Hello("Longoria")
+		expected := "Hello, Longoria"
+		assertCorrectMessage(t, actual, expected)
+	})
 
-	if got != want {
-		t.Errorf("got %q but want %q", got, want)
+	t.Run("saying 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		actual := Hello("")
+		expected := "Hello, World"
+		assertCorrectMessage(t, actual, expected)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, actual, expected string) {
+	/*
+		t.Helper() is needed to tell the test suite that this method is a helper.
+		By doing this when it fails the line number reported will be in our function call rather than inside our test helper.
+		This will help other developers track down problems easier.
+	*/
+	t.Helper()
+	if actual != expected {
+		t.Errorf("got %q but want %q", actual, expected)
 	}
 }
