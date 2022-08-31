9eab74931a6d3d63429097aae53b9e3f342dcc6e
Longoria Yu <longoria.yu@thecarousell.com>
2022-08-31 14:28:49 +0800

    Added name parameter in Hello function

--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	got := Hello("Longoria")
+	want := "Hello, Longoria"
 
 	if got != want {
 		t.Errorf("got %q but want %q", got, want)
