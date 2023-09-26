d55e2eedcc040750f5f4891c41275f627e9bab91
Longoria Yu <longoria.yu@thecarousell.com>
2023-09-26 18:33:50 +0800

    Use table-driven tests to test different shape types

--- struct_interface/shapes_test.go
+++ struct_interface/shapes_test.go
@@ -17,21 +17,22 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape si.Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	areaTests := []struct {
+		name  string
+		shape si.Shape
+		want  float64
+	}{
+		{name: "Rectangle", shape: si.Rectangle{Width: 12.0, Height: 6.0}, want: 72.0},
+		{name: "Circle", shape: si.Circle{Radius: 10.0}, want: 314.1592653589793},
+		{name: "Triangle", shape: si.Triangle{Base: 12, Height: 6}, want: 36.0},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := si.Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := si.Circle{10.0}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("%#v, got %g, want %g", tt.shape, got, tt.want)
+			}
+		})
+	}
 }
