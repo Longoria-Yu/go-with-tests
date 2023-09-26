24c6faa488d7cce11e409fbf9b11b199f1d00a79
Longoria Yu <longoria.yu@thecarousell.com>
2023-09-26 18:03:40 +0800

    Use struct and method instead of function

---
+++ struct_interface/shapes_test.go
@@ -0,0 +1,39 @@
+package shapes_test
+
+import (
+	"testing"
+
+	si "github.com/Longoria-Yu/go-with-tests/struct_interface"
+)
+
+func TestPerimeter(t *testing.T) {
+	rectangle := si.Rectangle{10.0, 10.0}
+	got := si.Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := si.Rectangle{12.0, 6.0}
+		got := rectangle.Area()
+		want := 72.0
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := si.Circle{10.0}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+}
